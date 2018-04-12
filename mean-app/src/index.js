306cf856f77e614bb4075c225730c881f79b25fc
alevegap92 <ravp_92@hotmail.com>
2018-04-12 18:17:11 -0300

    First Mean

---
+++ mean-app/src/index.js
@@ -0,0 +1,32 @@
+const cors = require('cors');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//Routers rutas
+//const indexRoutes = require('./routes/index');
+const tasksRoutes = require('./routes/tasks');
+
+// settings
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 3000);
+
+// middlewares: antes que se carge en el navegador y el cliente
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+//route
+//app.use(indexRoutes);
+app.use('/api',tasksRoutes);
+
+//static files
+app.use(express.static(path.join(__dirname, 'dist')))
+
+//start server
+app.listen(app.get('port'), ()=> {
+	console.log('server on port 3000', app.get('port'));
+});
+console.log('Funciona');
