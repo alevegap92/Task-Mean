4e02d11e1459112e4f35ae4eb8fef0676d6eb5d5
alevegap92 <ravp_92@hotmail.com>
2018-04-16 12:34:50 -0300

    CRUD Ionic

--- ionicTask/src/pages/home/home.ts
+++ ionicTask/src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { TasksProvider } from '../../providers/tasks/*tasks';
-import { Task } from '../../models/Task';
+import { Task } from '../../models/*Task';
 
 @Component({
   selector: 'page-home',
@@ -12,6 +12,7 @@ export class HomePage {
   constructor(public navCtrl: NavController, public taskService:TasksProvider) {
   	console.log('que pasa?asdasds')
   }
+
   ionViewDidLoad(){
     this.taskService.getTasks()
     .subscribe(
@@ -19,5 +20,49 @@ export class HomePage {
       error => this.error = error // error path
     );
   }
+
+  addTask(event){
+    event.preventDefault();
+    const newTask:Task = {
+      title: this.title,
+      isDone: false
+    }
+    this.taskService.addTask(newTask)
+      .subscribe(task => {
+        this.tasks.push(task);
+        this.title = '';
+      });     
+  }
+  
+  deleteTask(id) {
+    const response = confirm('are you sure to delete it?');
+    if (response ){
+      const tasks = this.tasks;
+      this.taskService.deleteTask(id)
+        .subscribe(data => {
+          console.log(data.n);
+          if(data.n == 1) {
+            for(let i = 0; i < tasks.length; i++) {
+              if(tasks[i]._id == id) {
+                tasks.splice(i, 1);
+              }
+            }
+          }
+        })
+    }
+  }                 
+
+  updateStatus(task: Task) {
+    var newTask = {            
+      _id: task._id,
+      title: task.title,
+      isDone: !task.isDone
+    };
+    this.taskService.updateTask(newTask)
+      .subscribe(res => {
+        task.isDone = !task.isDone;
+      })
+  }
+
 }
 
--- ionicTask/src/providers/tasks/*tasks.ts
+++ ionicTask/src/providers/tasks/*tasks.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
 import { catchError, retry } from 'rxjs/operators';
 
-import { Task } from '../../models/Task';
+import { Task } from '../../models/*Task';
 /*
   Generated class for the TasksProvider provider.
 
@@ -14,14 +15,42 @@ import { Task } from '../../models/Task';
 
 export class TasksProvider {
 
+  domain: string = "http://localhost:3000";
+
   constructor(public http: HttpClient) {
     console.log('Hello TasksProvider Provider');
   }
   getTasks() {
-    return this.http.get<Task>('http://localhost:3000/api/tasks')
+    return this.http.get<Task>(`${this.domain}/api/tasks`)
       .pipe(
       retry(3), // retry a failed request up to 3 times
       catchError(this.handleError) // then handle the error
       );
   }
+
+  /** POST: add a new task to the database */
+  addTask(task:Task): Observable<Task> {
+  	 return this.http.post<Task>(`${this.domain}/api/tasks`, newTask)
+    .pipe(
+      catchError(this.handleError('addTask', task))
+    );
+  }
+
+  /** DELETE: delete the task from the server */	
+  deleteTask (id): Observable<{}> {
+	  const url = `${this.domain}/api/tasks/${id}`; // example: DELETE api/heroes/42
+	  return this.http.delete(url)
+	    .pipe(
+	      catchError(this.handleError('deleteTask'))
+	    );
+  }
+
+  /** PUT: update the task on the server. Returns the updated task upon success. */
+  updateTask(task:Task): Observable<Task> {
+  return this.http.put<Task>(`${this.domain}/api/tasks`, newTask)
+    .pipe(
+      catchError(this.handleError('updateTask', Task))
+    );
+  }
+
 }
