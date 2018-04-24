7f77914de705ca0009df0b94b53fadfd60e3a3df
alevegap92 <ravp_92@hotmail.com>
2018-04-24 16:14:11 -0300

    FULL API v1.0

--- ionicTask/src/pages/home/home.ts
+++ ionicTask/src/pages/home/home.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';;
 import { TasksProvider } from '../../providers/tasks/*tasks';
-import { Task } from '../../models/*Task';
+import { Task } from '../../models/Task';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
+  
 })
 export class HomePage {
   tasks: Task;
-  constructor(public navCtrl: NavController, public taskService:TasksProvider) {
+  title: string;
+
+  constructor(public navCtrl: NavController, public taskService: TasksProvider ) {
   	console.log('que pasa?asdasds')
   }
 
@@ -17,43 +21,29 @@ export class HomePage {
     this.taskService.getTasks()
     .subscribe(
       data => this.tasks = data, // success path
-      error => this.error = error // error path
+
     );
   }
 
-  addTask(event){
-    event.preventDefault();
-    const newTask:Task = {
-      title: this.title,
-      isDone: false
-    }
-    this.taskService.addTask(newTask)
-      .subscribe(task => {
-        this.tasks.push(task);
-        this.title = '';
-      });     
-  }
-  
   deleteTask(id) {
     const response = confirm('are you sure to delete it?');
-    if (response ){
+    if (response) {
       const tasks = this.tasks;
       this.taskService.deleteTask(id)
         .subscribe(data => {
           console.log(data.n);
-          if(data.n == 1) {
-            for(let i = 0; i < tasks.length; i++) {
-              if(tasks[i]._id == id) {
+          if (data.n == 1) {
+            for (let i = 0; i < tasks.length; i++) {
+              if (tasks[i]._id == id) {
                 tasks.splice(i, 1);
               }
             }
           }
         })
     }
-  }                 
-
+  }  
   updateStatus(task: Task) {
-    var newTask = {            
+    var newTask = {
       _id: task._id,
       title: task.title,
       isDone: !task.isDone
@@ -62,6 +52,19 @@ export class HomePage {
       .subscribe(res => {
         task.isDone = !task.isDone;
       })
+  }
+  
+  addTask(event) {
+    event.preventDefault();
+    const newTask: Task = {
+      title: this.title,
+      isDone: false
+    }
+    this.taskService.addTask(newTask)
+      .subscribe(task => {
+        this.tasks.push(task);
+        this.title = '';
+      });
   }
 
 }
--- ionicTask/src/providers/tasks/*tasks.ts
+++ ionicTask/src/providers/tasks/*tasks.ts
@@ -1,56 +1,44 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-
-import { Observable } from 'rxjs/Observable';
-import { catchError, retry } from 'rxjs/operators';
-
-import { Task } from '../../models/*Task';
+import { Injectable, } from '@angular/core';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import "rxjs/Rx";
+import { Task } from '../../models/Task';
 /*
   Generated class for the TasksProvider provider.
 
   See https://angular.io/guide/dependency-injection for more info on providers
   and Angular DI.
 */
-@Injectable()
 
+
+
+@Injectable()
 export class TasksProvider {
 
   domain: string = "http://localhost:3000";
+  tasksUrl: string = 'http://localhost:3000/api/tasks';
 
   constructor(public http: HttpClient) {
     console.log('Hello TasksProvider Provider');
   }
+
   getTasks() {
     return this.http.get<Task>(`${this.domain}/api/tasks`)
-      .pipe(
-      retry(3), // retry a failed request up to 3 times
-      catchError(this.handleError) // then handle the error
-      );
+   
   }
 
-  /** POST: add a new task to the database */
-  addTask(task:Task): Observable<Task> {
-  	 return this.http.post<Task>(`${this.domain}/api/tasks`, newTask)
-    .pipe(
-      catchError(this.handleError('addTask', task))
-    );
+  deleteTask(id) {
+    return this.http.delete<Task>(`${this.domain}/api/tasks/${id}`)
+      .map(res => res);
   }
 
-  /** DELETE: delete the task from the server */	
-  deleteTask (id): Observable<{}> {
-	  const url = `${this.domain}/api/tasks/${id}`; // example: DELETE api/heroes/42
-	  return this.http.delete(url)
-	    .pipe(
-	      catchError(this.handleError('deleteTask'))
-	    );
+  addTask(newTask: Task) {
+    return this.http.post<Task>(`${this.domain}/api/tasks`, newTask)
+      .map(res => res);
   }
-
-  /** PUT: update the task on the server. Returns the updated task upon success. */
-  updateTask(task:Task): Observable<Task> {
-  return this.http.put<Task>(`${this.domain}/api/tasks`, newTask)
-    .pipe(
-      catchError(this.handleError('updateTask', Task))
-    );
+  updateTask(newTask) {
+    return this.http.put<Task>(`${this.domain}/api/tasks/${newTask._id}`, newTask)
+      .map(res => res)
   }
-
 }
